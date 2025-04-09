2ebc7a16339b75bf0ad670e5635c0af009f4212b
microwalker <microwalker@gmx.net>
2025-04-09 19:27:20 +0200

    delete + add & some optical improvements

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,22 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Column(
+        body: SafeArea(child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              MyTolleListe(), 
-              
+              SizedBox(height: 48),
+              Card(margin: EdgeInsets.all(16), elevation: 8, child: MyTolleListe()), 
+              Text("...ist alles was man braucht 😜!")
             ])
-        ),
+          )
+        )
       ),
     );
   }
 }
 
 class MyTolleListe extends StatefulWidget {
+  final List<String> listeImWidget = const ["Bier", "Wein", "Sekt", "Champagner", "Schnaps", "Whiskey", "Tequila", "Wodka"];
   const MyTolleListe({super.key});
 
   @override
@@ -38,25 +41,44 @@ class MyTolleListe extends StatefulWidget {
 }
 
 class _MyTolleListeState extends State<MyTolleListe> {
-  List<String> listeImState = ["Bier", "Wein", "Sekt", "Schnaps", "Whiskey"];
+  List<String> listeImState = []; 
+
+  @override
+  void initState() {
+    listeImState = [ ...widget.listeImWidget ];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return 
-    Column(
+    Column(mainAxisAlignment: MainAxisAlignment.spaceAround, spacing: 16,
       children: [
+        SizedBox(height: 12),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          ElevatedButton(onPressed: () { iDeleteTheLastPosition(); }, child: Text("Letzte Position löschen!")),
+          ElevatedButton(onPressed: () { iRestoreTheNextPosition(); }, child: Text("Liste wieder erweitern!"))]),              
         ListView.builder(
           shrinkWrap: true,
           itemCount: listeImState.length,
-          itemBuilder: (context, index) => ListTile(title: Text(listeImState[index]),) ),
-          ElevatedButton(onPressed: () { iDeleteTheLastPosition(); }, child: Text("Letzte Position löschen!"))      
+          itemBuilder: (context, index) => ListTile(onTap: () {}, leading: Icon(Icons.liquor_outlined), trailing: Icon(Icons.medical_services), title: Text(listeImState[index]))),
       ],
     );
   }
 
   // Korrekt:
-  void iDeleteTheLastPosition() { setState(() { 
-    listeImState.removeLast(); } ); 
+  void iDeleteTheLastPosition() { 
+    setState(() { 
+      if(listeImState.isNotEmpty) {
+        listeImState.removeLast(); }}); 
+  }
+
+  void iRestoreTheNextPosition() {
+    setState(() {
+      if(listeImState.length < widget.listeImWidget.length) {
+        listeImState.add(widget.listeImWidget[listeImState.length]);
+      } 
+    });
   }
 
   // FALSCH:
